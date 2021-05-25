20ec6410f2d82f4ffa8ee1e94e0c5cc0c3056700
Gabriel Wright <wrightgabriel0220@gmail.com>
2021-05-25 12:23:22 -0700

    Implemented POST route server-side and JSON input form client-side

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.sendFile('./index.html', {root: './'});
 });
+
+app.post('/', function(req, res) {
+  console.log(`Got a POST request: ${req.body.jsonInput}`);
+  res.redirect('/');
+})
 
 app.listen(3000);
 console.log('LISTENING ON PORT 3000');
